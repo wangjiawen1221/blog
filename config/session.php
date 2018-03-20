0d1811350dc8b8f34dc47dd2480513a27a8ac79d
wangjiawen <wangjiawen1992@qq.com>
2018-03-20 10:20:00 +0800

    设置登入过期时间

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 60*24*30),
+    'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
