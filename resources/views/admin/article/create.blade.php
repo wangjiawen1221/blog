d3b49aa3f9067d85dbd8775216f317fe24f58041
root <291120569@qq.com>
2018-07-30 09:43:58 +0800

    213

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -71,9 +71,19 @@
             <tr>
                 <th>内容</th>
                 <td>
-                    <div id="bjy-content">
-                        <textarea name="markdown">{{ old('markdown') }}</textarea>
+                    <div class="edui-default">
+                        @include('UEditor::head')
+                        <script id="ueditor" name="markdown" type="text/plain" style='width:100%;height:500px;'></script>
+                        <script type="text/javascript">
+                            var ue = UE.getEditor('ueditor');
+                            ue.ready(function(){
+                                ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); 
+                            });
+                        </script>
                     </div>
+                    <!-- <div id="bjy-content">
+                        <textarea name="markdown">{{ old('markdown') }}</textarea>
+                    </div> -->
                 </td>
             </tr>
             <tr>
